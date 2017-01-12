e46d6d93fdc6670d05da32dee1b261f9305f5e24
Josh S <josh@joshsomma.com>
2017-01-12 15:49:46 +0000

    initial commit for webcrawler

---
+++ general.py
@@ -0,0 +1,9 @@
+import os
+
+# each website you crawl is a separate project (folder)
+def create_project_dir(directory):
+    if not os.path.exists(directory):
+        print('Creating project: ' + directory)
+        os.makedirs(directory)
+
+create_project_dir('joshskimo')
